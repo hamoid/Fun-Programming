d6b981d8e6ff442a66d9f237ae5cd8c18485fd0e
Abe Pazos <hamoid@gmail.com>
2012-05-17 11:25:57 +0300

    There. Absolute path will absolutely fix the links.

--- processing/build_readme.py
+++ processing/build_readme.py
@@ -10,12 +10,11 @@ import os, os.path, subprocess
 execstr = "git ls-files ./ | grep -E '.pde|.scd'"
 result = subprocess.check_output(execstr, shell=True).split('\n')
 
-#currDir = os.getcwd().split('/')[-1]
+absolutePath = 'https://github.com/hamoid/Fun-Programming/blob/master/processing'
 
 f = open('readme.md', "w")
 for line in result:
   if line:
-    f.write('[%s](%s)  \n' % (os.path.basename(line), line))
-    #f.write('[%s](%s/%s)  \n' % (os.path.basename(line), currDir, line))
+    f.write('[%s](%s/%s)  \n' % (os.path.basename(line), absolutePath, line))
 f.close()
 
