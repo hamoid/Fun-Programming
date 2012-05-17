4238f37a495ffa69d77a993b970fc09e73560275
Abe Pazos <hamoid@gmail.com>
2012-05-17 11:03:40 +0300

    Fix readme script, was missing current dir

--- processing/build_readme.py
+++ processing/build_readme.py
@@ -1,11 +1,14 @@
-import os.path, subprocess
+import os, os.path, subprocess
 
 
 execstr = "git ls-files ./ | grep -E '.pde|.scd'"
 result = subprocess.check_output(execstr, shell=True).split('\n')
 
+currDir = os.getcwd().split('/')[-1]
+
 f = open('readme.md', "w")
 for line in result:
   if line:
-    f.write('[%s](%s)\n' % (os.path.basename(line), line))
+    f.write('[%s](%s/%s)\n' % (os.path.basename(line), currDir, line))
 f.close()
+
