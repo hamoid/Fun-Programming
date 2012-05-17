5a2ed77abe04e24cd602638902dd058c7aaa5606
Abe Pazos <hamoid@gmail.com>
2012-05-17 11:11:20 +0300

    Add a description to the build_readme.py script

--- processing/build_readme.py
+++ processing/build_readme.py
@@ -1,5 +1,11 @@
-import os, os.path, subprocess
+# This Python script finds recursively all
+# Processing and SuperCollider programs
+# under the current directory that have
+# been added to Git, then saves the
+# resulting list to the file "readme.md",
+# which GitHub uses as a read me file.
 
+import os, os.path, subprocess
 
 execstr = "git ls-files ./ | grep -E '.pde|.scd'"
 result = subprocess.check_output(execstr, shell=True).split('\n')
