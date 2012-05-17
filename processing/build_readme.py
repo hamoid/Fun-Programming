d2cd9bf60b960ba3f4a727e40d5f8b106cdcd917
Abe Pazos <hamoid@gmail.com>
2012-05-17 11:22:43 +0300

    Somehow the fix-path thing did not work. Removing current dir again

--- processing/build_readme.py
+++ processing/build_readme.py
@@ -10,11 +10,12 @@ import os, os.path, subprocess
 execstr = "git ls-files ./ | grep -E '.pde|.scd'"
 result = subprocess.check_output(execstr, shell=True).split('\n')
 
-currDir = os.getcwd().split('/')[-1]
+#currDir = os.getcwd().split('/')[-1]
 
 f = open('readme.md', "w")
 for line in result:
   if line:
-    f.write('[%s](%s/%s)  \n' % (os.path.basename(line), currDir, line))
+    f.write('[%s](%s)  \n' % (os.path.basename(line), line))
+    #f.write('[%s](%s/%s)  \n' % (os.path.basename(line), currDir, line))
 f.close()
 
